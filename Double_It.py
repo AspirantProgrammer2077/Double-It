d3238187ae41181570eb3089a6214a0dc1dfeda6
AspirantProgrammer2077 <andreidom907@gmail.com>
2025-08-03 22:19:25 +0800

    Add files via upload

---
+++ Double_It.py
@@ -0,0 +1,9 @@
+def main():
+    curr_value = int(input("Enter a number: "))
+    greater_than = 100
+    while curr_value < greater_than:
+        curr_value = curr_value * 2
+        print(curr_value)
+
+if __name__ == '__main__':
+    main()
